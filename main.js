2453239275909393c9ef313d8c568ade85cb01b3
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-06-19 17:57:06 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,198 @@
+"use strict";
+const { default: makeWASocket, DisconnectReason, useMultiFileAuthState, makeInMemoryStore, downloadContentFromMessage, jidDecode, generateForwardMessageContent, generateWAMessageFromContent } = require("@adiwajshing/baileys")
+const fs = require("fs");
+const figlet = require("figlet");
+const lolcatjs = require('lolcatjs');
+const chalk = require('chalk')
+const logg = require('pino')
+const Jimp = require('jimp') 
+const clui = require('clui')
+const { Spinner } = clui
+const { serialize, fetchJson, getBuffer } = require("./function/func_Server");
+const { nocache, uncache } = require('./function/Chache_Data.js');
+const { welcome_JSON } = require('./function/Data_Location.js')
+const { status_Connection } = require('./function/Data_Server_Bot/Status_Connect.js')
+const { Memory_Store } = require('./function/Data_Server_Bot/Memory_Store.js')
+const { groupResponse_Welcome, groupResponse_Remove, groupResponse_Promote, groupResponse_Demote } = require('./function/group_Respon.js')
+const { imageToWebp, videoToWebp, writeExifImg, writeExifVid } = require('./function/Exif_Write')
+const { updateGroup } = require("./function/update_Group")
+
+let setting = JSON.parse(fs.readFileSync('./config.json'));
+let session = `./${setting.sessionName}.json`
+
+function title() {
+	console.clear()
+	console.log('----------------------------------------------------')
+	lolcatjs.fromString(chalk.cyan(figlet.textSync('Rafly', {
+		font: 'Bloody',
+		horizontalLayout: 'full',
+		verticalLayout: 'full',
+		whitespaceBreak: true
+	})));
+	console.log('----------------------------------------------------')
+	lolcatjs.fromString('[SERVER STARTED!!!]')
+	console.log('----------------------------------------------------')
+	lolcatjs.fromString('Create by Rafly¹¹')
+	console.log('----------------------------------------------------')
+}
+
+const status = new Spinner(chalk.cyan(` Booting WhatsApp Bot`))
+const starting = new Spinner(chalk.cyan(` Preparing After Connect`))
+const reconnect = new Spinner(chalk.redBright(` Reconnecting WhatsApp Bot`))
+
+async function sedative() {
+	const { state, saveCreds } = await useMultiFileAuthState('./sessions')
+
+	async function connectToWhatsApp() {
+
+		const conn = makeWASocket({
+			printQRInTerminal: true,
+			markOnlineOnConnect: false,
+			logger: logg({ level: 'fatal' }),
+			browser: ['SEDATIVE MD-Rafly11', 'Safari', '1.0.0'],
+			auth: state,
+			generateHighQualityLinkPreview: true,
+			patchMessageBeforeSending: (message) => {
+                const requiresPatch = !!(
+                    message.buttonsMessage 
+                    || message.templateMessage
+                    || message.listMessage
+                );
+				if (requiresPatch) {
+                    message = {
+                        viewOnceMessage: {
+                            message: {
+                                messageContextInfo: {
+                                    deviceListMetadataVersion: 2,
+                                    deviceListMetadata: {},
+                                },
+                                ...message,
+                            },
+                        },
+                    };
+                }
+				return message;
+			}
+		})
+		title()
+		Memory_Store.bind(conn.ev)
+
+		conn.ev.on('messages.upsert', async m => {
+			var msg = m.messages[0]
+			if (!m.messages) return;
+			if (msg.key && msg.key.remoteJid == "status@broadcast") return
+			msg = serialize(conn, msg)
+			msg.isBaileys = msg.key.id.startsWith('BAE5') || msg.key.id.startsWith('3EB0')
+			require('./conn')(conn, msg, m, setting, Memory_Store)
+		})
+
+		conn.ev.on('creds.update', saveCreds)
+
+		conn.reply = (from, content, msg) => conn.sendMessage(from, { text: content }, { quoted: msg })
+
+
+		conn.ev.on('connection.update', (update) => {
+			status_Connection(conn, update, connectToWhatsApp)
+		})
+
+		conn.ev.on('group-participants.update', async (update) => {
+			console.log(update)
+		})
+
+		conn.ev.on('group-update', async (anu) => {
+			updateGroup(conn, anu, MessageType)
+		})
+
+		conn.sendImage = async (jid, path, caption = '', quoted = '', options) => {
+			let buffer = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
+			return await conn.sendMessage(jid, { image: buffer, caption: caption, ...options }, { quoted })
+		}
+
+		conn.decodeJid = (jid) => {
+			if (!jid) return jid
+			if (/:\d+@/gi.test(jid)) {
+				let decode = jidDecode(jid) || {}
+				return decode.user && decode.server && decode.user + '@' + decode.server || jid
+			} else return jid
+		}
+
+         conn.generateProfilePicture = async (buffer) => {
+
+	const jimp = await Jimp.read(buffer)
+	const min = jimp.getWidth()
+	const max = jimp.getHeight()
+	const cropped = jimp.crop(0, 0, min, max)
+	return {
+		img: await cropped.scaleToFit(720, 720).getBufferAsync(Jimp.MIME_JPEG),
+		preview: await cropped.scaleToFit(720, 720).getBufferAsync(Jimp.MIME_JPEG)
+	          }
+         }
+		conn.downloadAndSaveMediaMessage = async (msg, type_file, path_file) => {
+			if (type_file === 'image') {
+				var stream = await downloadContentFromMessage(msg.message.imageMessage || msg.message.extendedTextMessage?.contextInfo.quotedMessage.imageMessage, 'image')
+				let buffer = Buffer.from([])
+				for await (const chunk of stream) {
+					buffer = Buffer.concat([buffer, chunk])
+				}
+				fs.writeFileSync(path_file, buffer)
+				return path_file
+			} else if (type_file === 'video') {
+				var stream = await downloadContentFromMessage(msg.message.videoMessage || msg.message.extendedTextMessage?.contextInfo.quotedMessage.videoMessage, 'video')
+				let buffer = Buffer.from([])
+				for await (const chunk of stream) {
+					buffer = Buffer.concat([buffer, chunk])
+				}
+				fs.writeFileSync(path_file, buffer)
+				return path_file
+			} else if (type_file === 'sticker') {
+				var stream = await downloadContentFromMessage(msg.message.stickerMessage || msg.message.extendedTextMessage?.contextInfo.quotedMessage.stickerMessage, 'sticker')
+				let buffer = Buffer.from([])
+				for await (const chunk of stream) {
+					buffer = Buffer.concat([buffer, chunk])
+				}
+				fs.writeFileSync(path_file, buffer)
+				return path_file
+			} else if (type_file === 'audio') {
+				var stream = await downloadContentFromMessage(msg.message.audioMessage || msg.message.extendedTextMessage?.contextInfo.quotedMessage.audioMessage, 'audio')
+				let buffer = Buffer.from([])
+				for await (const chunk of stream) {
+					buffer = Buffer.concat([buffer, chunk])
+				}
+				fs.writeFileSync(path_file, buffer)
+				return path_file
+			}
+		}
+		conn.sendImageAsSticker = async (jid, path, quoted, options = {}) => {
+			let buff = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
+			let buffer
+			if (options && (options.packname || options.author)) {
+				buffer = await writeExifImg(buff, options)
+			} else {
+				buffer = await imageToWebp(buff)
+			}
+			await conn.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted })
+				.then(response => {
+					fs.unlinkSync(buffer)
+					return response
+				})
+		}
+
+		conn.sendVideoAsSticker = async (jid, path, quoted, options = {}) => {
+			let buff = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
+			let buffer
+			if (options && (options.packname || options.author)) {
+				buffer = await writeExifVid(buff, options)
+			} else {
+				buffer = await videoToWebp(buff)
+			}
+			await conn.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted })
+				.then(response => {
+					fs.unlinkSync(buffer)
+					return response
+				})
+		}
+		return conn
+	}
+	connectToWhatsApp()
+}
+sedative()
