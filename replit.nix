e9e2a0e6bc08a83f36e2d23752aa077ca5b19100
Rafly11~ <75292071+dragneel1111@users.noreply.github.com>
2023-06-29 23:47:33 +0700

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -3,13 +3,7 @@
         pkgs.nodejs
         pkgs.nodePackages.typescript
         pkgs.ffmpeg
-        pkgs.imagemagick
         pkgs.git
-        pkgs.neofetch
-        pkgs.libwebp
-        pkgs.speedtest-cli
-        pkgs.wget
-        pkgs.yarn
         pkgs.libuuid
     ];
     env = {
