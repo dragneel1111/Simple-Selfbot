1d131dc4bd6bd8ab565976cc2d5d17edc0c97dd8
Rafly11~ <75292071+dragneel1111@users.noreply.github.com>
2023-07-16 21:20:59 +0000

    Add files via upload

--- function/Data_Location.js
+++ function/Data_Location.js
@@ -3,12 +3,10 @@ const fs = require('fs')
 
 JSON_DATA = {
 setting: JSON.parse(fs.readFileSync('./config.json')),
-mess: JSON.parse(fs.readFileSync('./database/message.json')),
 server_eror: JSON.parse(fs.readFileSync('./database/func_error.json')),
 db_respon_list: JSON.parse(fs.readFileSync('./database/db_addlist.json')),
 }
 
 exports.setting_JSON = JSON_DATA.setting;
-exports.mess_JSON = JSON_DATA.mess;
 exports.server_eror_JSON = JSON_DATA.server_eror;
 exports.db_respon_list_JSON = JSON_DATA.db_respon_list;
